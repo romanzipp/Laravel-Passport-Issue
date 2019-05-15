169c6376d963d85fb06681a831f4b5b0ec74e851
Roman Zipp <ich@ich.wtf>
2019-05-15 13:48:32 +0200

    Update AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use Laravel\Passport\Passport;
+use ReflectionClass;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +27,40 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        $this->app['router']->matched(function (\Illuminate\Routing\Events\RouteMatched $event) {
+
+            if ( ! in_array('api', $event->route->middleware())) {
+                return;
+            }
+
+            $this->app['auth']->shouldUse('api');
+
+            if ( ! request()->headers->has('Authorization') && request()->user()) {
+
+                //
+                // +++++++++++++++++++++++++++++++++++++++++
+                // COMMENT THE FOLLOWING LINE FOR DEBUG DUMP
+                return;
+                // +++++++++++++++++++++++++++++++++++++++++
+                //
+
+                $reflection = new ReflectionClass($this->app['auth']);
+
+                $property = $reflection->getProperty('guards');
+                $property->setAccessible(true);
+
+                $guards = $property->getValue($this->app['auth']);
+
+                dd([
+                    'authenticated-before' => $guards['api']->check(),
+                    'authenticated-after'  => $guards['api']->validate(['request' => request()]),
+                ]);
+            }
+        });
+
+        Passport::routes();
+
+        Passport::tokensExpireIn(now()->addDays(30));
+        Passport::refreshTokensExpireIn(now()->addMonths(6));
     }
 }
