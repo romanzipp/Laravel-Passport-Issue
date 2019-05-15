7cefd68cb73f9cc6afe10151bd9c83f0a010f2b9
Roman Zipp <ich@ich.wtf>
2019-05-15 13:44:57 +0200

    Add api route

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware(['api', 'auth:api'])->get('/route-that-requires-valid-auth', function (Request $request) {
     return $request->user();
 });
